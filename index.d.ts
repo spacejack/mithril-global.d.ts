42187eedaa0a03247dc329cb1395c738e1cee418
spacejack <spacejack@gmail.com>
2017-03-26 22:32:03 -0400

    UMD style types for script/global use

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,52 @@
-// Type definitions for mithril.js (global) 1.0
-// Project: https://github.com/lhorie/mithril.js
-// Definitions by: Mike Linkovich <https://github.com/spacejack>
+// Type definitions for Mithril 1.1
+// Project: http://lhorie.github.io/mithril/
+// Definitions by: Leo Horie <https://github.com/lhorie>, Chris Bowdon <https://github.com/cbowdon>, Mike Linkovich <https://github.com/spacejack>, András Parditka <https://github.com/andraaspar>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare const m: Mithril.Static;
+// Global Mithril types
+
+import * as mithril from 'mithril';
+import * as stream from 'mithril/stream';
+
+declare namespace MithrilGlobal {
+	export type Lifecycle<A,S> = mithril.Lifecycle<A,S>;
+	export type Hyperscript = mithril.Hyperscript;
+	export type RouteResolver<S,P> = mithril.RouteResolver<S,P>;
+	export type RouteDefs = mithril.RouteDefs;
+	export type RouteOptions = mithril.RouteOptions;
+	export type Route = mithril.Route;
+	export type Mount = mithril.Mount;
+	export type WithAttr = mithril.WithAttr;
+	export type ParseQueryString = mithril.ParseQueryString;
+	export type BuildQueryString = mithril.BuildQueryString;
+	export type RequestOptions<T> = mithril.RequestOptions<T>;
+	export type RequestOptionsAll<T> = mithril.RequestOptionsAll<T>;
+	export type Request = mithril.Request;
+	export type JsonpOptions = mithril.JsonpOptions;
+	export type JsonpOptionsAll = mithril.JsonpOptionsAll;
+	export type Jsonp = mithril.Jsonp;
+	export type RequestService = mithril.RequestService;
+	export type Render = mithril.Render;
+	export type RenderService = mithril.RenderService;
+	export type Redraw = mithril.Redraw;
+	export type RedrawService = mithril.RedrawService;
+	export type Child = mithril.Child;
+	export type ChildArray = mithril.ChildArray;
+	export type Children = mithril.Children;
+	export type ChildArrayOrPrimitive = mithril.ChildArrayOrPrimitive;
+	export type Vnode<A,S> = mithril.Vnode<A,S>;
+	export type VnodeDOM<A,S> = mithril.VnodeDOM<A,S>;
+	export type CVnode<A> = mithril.CVnode<A>;
+	export type CVnodeDOM<A> = mithril.CVnodeDOM<A>;
+	export type Component<A,S> = mithril.Component<A,S>;
+	export type Comp<A,S> = mithril.Comp<A,S>
+	export type ClassComponent<A> = mithril.ClassComponent<A>;
+	export type FactoryComponent<A> = mithril.FactoryComponent<A>;
+	export type Attributes = mithril.Attributes;
+	export type Stream<T> = stream.Stream<T>;
+	export type Static = mithril.Static & {stream: stream.Static};
+}
+
+declare const MithrilGlobal: MithrilGlobal.Static;
+export = MithrilGlobal;
+export as namespace m;
--- mithril-global-tests.ts
+++ mithril-global-tests.ts
@@ -1,9 +1,13 @@
-// Just test the global mithril objects
+// Test global mithril types
 
 const comp = {
 	view() {
 		return m('span', "Test")
 	}
-}
+} as m.Comp<{},{}>
 
-m.mount(document.getElementById('comp'), comp)
+m.mount(document.getElementById('comp')!, comp)
+
+const vnode = m('div', 'Test')
+
+const s: m.Stream<number> = m.stream(1)
